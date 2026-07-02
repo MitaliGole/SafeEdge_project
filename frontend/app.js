2f98fad12be88f020aae913f383a25872b1080da
Mitali Gole <mitaligole02@gmail.com>
2026-07-03 01:35:30 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const API = "https://safeedge-project-1.onrender.com"; // Change this to your backend URL if needed
+const API = "https://safeedge-project-o2lu.onrender.com";// Change this to your backend URL if needed
 const $   = id => document.getElementById(id);
 
 // ── State
